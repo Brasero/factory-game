e0bd751fe565b71b2b55f69b231e92f78b845db7
brasero <brandonricci4@gmail.com>
2026-09-16 14:20:50 +0200

    fix(Production system): modification du système de calcul de stockage, on ne compte que le stockage de la ressource produite en ignorant les ressource d'entrée

--- packages/engine/systems/ProductionSystem.ts
+++ packages/engine/systems/ProductionSystem.ts
@@ -13,12 +13,12 @@ export function runProduction(world: World): World {
             if (progress < recipe.duration) {
                 return {...m, buffer, progress, active: true};
             }
-            buffer[recipe.input] = (buffer[recipe.input] ?? 1) - 1;
-            buffer[recipe.output] = (buffer[recipe.output] ?? 0) + m.production;
-            const totalStored = Object.values(buffer).reduce((a,b) => a+b, 0);
+            const totalStored = buffer[recipe.output] || 0;
             if ((buffer[recipe.input] ?? 0) <= 0 || totalStored >= m.capacity) {
                 return {...m, active: false};
             }
+            buffer[recipe.input] = (buffer[recipe.input] ?? 1) - 1;
+            buffer[recipe.output] = (buffer[recipe.output] ?? 0) + m.production;
             return {...m, buffer, progress: 0, active: true};
         }
         if (
